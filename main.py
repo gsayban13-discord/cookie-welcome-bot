02d9a37177bfff7c9765c175834db6b4406ee071
gsayban13-discord <gsayban13@gmail.com>
2026-03-20 01:05:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="?", intents=intents)
 
 
 # ---------- DATABASE ----------
